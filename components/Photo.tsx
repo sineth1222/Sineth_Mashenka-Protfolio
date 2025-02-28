f2cb28b4fae9783f7d19884e7d32a84a93b9cfe7
sineth1222 <106462252+sineth1222@users.noreply.github.com>
2025-02-28 19:42:40 +0530

    profile picture update

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -9,7 +9,7 @@ const Photo = () => {
         {/* image */}
       <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: {delay: 2, duration: 0.4, ease: "easeIn"}, }}>
         <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: {delay: 2, duration: 0.4, ease: "easeIn"}, }} className="w-[298px] h-[298px] xl:h-[498px] xl:w-[498px] mix-blend-lighten absolute">
-            <Image src="/assets/photo3.png" priority quality={100} fill alt="" className="object-contain rounded-full"/>
+            <Image src="/assets/photo5.png" priority quality={100} fill alt="" className="object-contain rounded-full"/>
         </motion.div>
         {/* circle */}
         <motion.svg className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]" fill="transparent" viewBox="0 0 506 506" xmlns="http://www.w3.org/2000/svg">
